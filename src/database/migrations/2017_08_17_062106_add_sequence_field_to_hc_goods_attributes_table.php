431da31f196afe86a85d6b5483d46d220322cee5
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-17 10:47:37 +0300

    changed to default 0 value

--- src/database/migrations/2017_08_17_062106_add_sequence_field_to_hc_goods_attributes_table.php
+++ src/database/migrations/2017_08_17_062106_add_sequence_field_to_hc_goods_attributes_table.php
@@ -14,7 +14,7 @@ class AddSequenceFieldToHcGoodsAttributesTable extends Migration
     public function up()
     {
         Schema::table('hc_goods_attributes', function (Blueprint $table) {
-            $table->integer('sequence')->nullable();
+            $table->integer('sequence')->default(0);
         });
     }
 
