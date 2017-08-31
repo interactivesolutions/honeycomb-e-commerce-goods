71af8c91808904e3d7c77c8d66abfafd8ae57a4c
Mantas Paulauskas <mantopastas@gmail.com>
2017-08-31 11:55:03 +0300

    added helper function for converting numbers to words

--- src/app/helpers/PriceHelper.php
+++ src/app/helpers/PriceHelper.php
@@ -146,4 +146,102 @@ class PriceHelper
             (100 - ($finalPrice * 100 / $originalPrice)), $precision
         );
     }
+
+    /**
+     * Convert number to words
+     *
+     * @link https://gist.github.com/evaqas/63fa204e3a1f0019bce2
+     * @param $skaicius
+     * @return string|void
+     */
+    public static function numberToWordLt($skaicius)
+    {
+        // neskaiciuosim neigiamu ir itin dideliu skaiciu (iki milijardu)
+        if( $skaicius < 0 || strlen($skaicius) > 9 ) return;
+
+        if( $skaicius == 0 ) return 'nulis';
+
+        $vienetai = ['', 'vienas', 'du', 'trys', 'keturi', 'penki', 'šeši', 'septyni', 'aštuoni', 'devyni'];
+        $niolikai = ['', 'vienuolika', 'dvylika', 'trylika', 'keturiolika', 'penkiolika', 'šešiolika', 'septyniolika', 'aštuoniolika', 'devyniolika'];
+        $desimtys = ['', 'dešimt', 'dvidešimt', 'trisdešimt', 'keturiasdešimt', 'penkiasdešimt', 'šešiasdešimt', 'septyniasdešimt', 'aštuoniasdešimt', 'devyniasdešimt'];
+        $pavadinimas = [
+            ['milijonas', 'milijonai', 'milijonų'],
+            ['tūkstantis', 'tūkstančiai', 'tūkstančių'],
+        ];
+
+        $skaicius = sprintf('%09d', $skaicius); // iki milijardu 10^9 (milijardu neskaiciuosim)
+        $skaicius = str_split($skaicius, 3); // kertam kas tris simbolius
+
+        $zodziais = [];
+
+        foreach ( $skaicius as $i => $tripletas ) {
+            // resetinam linksni
+            $linksnis = 0;
+
+            // pridedam simtu pavadinima, jei pirmas tripleto skaitmuo > 0
+            if( $tripletas{0} > 0 ) {
+                $zodziais[] = $vienetai[$tripletas{0}];
+                $zodziais[] = ($tripletas{0} > 1) ? 'šimtai' : 'šimtas';
+            }
+
+            // du paskutiniai tripleto skaiciai
+            $du = substr($tripletas, 1);
+
+            // pacekinam nioliktus skaicius
+            if( $du > 10 && $du < 20 ) {
+                $zodziais[] = $niolikai[$du{1}];
+                $linksnis = 2;
+            } else {
+                // pacekinam desimtis
+                if( $du{0} > 0 ) {
+                    $zodziais[] = $desimtys[$du{0}];
+                }
+                // pridedam vienetus
+                if( $du{1} > 0 ) {
+                    $zodziais[] = $vienetai[$du{1}];
+                    $linksnis = ($du{1} > 1) ? 1 : 0;
+                } else {
+                    $linksnis = 2;
+                }
+            }
+
+            // pridedam pavadinima isskyrus paskutiniam ir nuliniams tripletams
+            if( $i < count($pavadinimas) && $tripletas != '000' ) {
+                $zodziais[] = $pavadinimas[$i][$linksnis];
+            }
+        }
+
+        return implode(' ', $zodziais);
+    }
+
+    /**
+     * Currency word ending
+     *
+     * @param $number
+     * @param string $saknis
+     * @return string|void
+     */
+    public static function currencyEndingInLt($number, $saknis = 'eur')
+    {
+        if( $number < 0 || strlen($number) > 9 ) return;
+
+        if( $number == 0 ) {
+            return $saknis . 'ų';
+        }
+
+        $last = substr($number, -1);
+        $du = substr($number, -2, 2);
+
+        if( ($du > 10) && ($du < 20) ) {
+            return $saknis . 'ų';
+        } else {
+            if( $last == 0 ) {
+                return $saknis . 'ų';
+            } elseif( $last == 1 ) {
+                return $saknis . 'as';
+            } else {
+                return $saknis . 'ai';
+            }
+        }
+    }
 }
